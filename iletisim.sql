5cc2f7686c5507f89db4e97e99c2f2822cb4057d
NisaAtaizi <nisaataizi05@gmail.com>
2025-01-17 20:22:58 +0300

    mysql veritabanı

---
+++ iletisim.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 17 Oca 2025, 18:12:53
+-- Sunucu sürümü: 10.4.32-MariaDB
+-- PHP Sürümü: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `iletisim`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `iletisim`
+--
+
+CREATE TABLE `iletisim` (
+  `id` int(11) NOT NULL,
+  `ad` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `mesaj` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `iletisim`
+--
+
+INSERT INTO `iletisim` (`id`, `ad`, `email`, `mesaj`) VALUES
+(9, 'nisa ataizi', 'nisaataizi@gmail.com', 'bu bir deneme mesajıdır'),
+(10, 'aleyna', 'aleyna@gmail.com', 'merhaba'),
+(13, 'güler', 'gunesgu26@gmail.com', 'çiçekleriniz çok güzel teşekkürler'),
+(15, 'deneme', 'example@gmail.com', 'deneme mesajı');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `iletisim`
+--
+ALTER TABLE `iletisim`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `iletisim`
+--
+ALTER TABLE `iletisim`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
